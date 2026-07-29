82c1c06a575b93a13c0813946657b225bfc2a4b9
otaviobrignoni <otavio.678.b@gmail.com>
2026-07-29 08:30:34 -0300

    altera program para usar padrão Program.Main()

--- src/GeradorDeProvas.WebApp/Program.cs
+++ src/GeradorDeProvas.WebApp/Program.cs
@@ -5,42 +5,50 @@ using GeradorDeProvas.WebApp.Compartilhado;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace GeradorDeProvas.WebApp;
 
-// Configuração do container de injeção de dependência
-builder.Services.AddInfraRepositories(builder.Configuration, builder.Logging);
-builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddPresentationConfig(builder.Configuration);
-
-// Configura health checks do banco de dados
-builder.Services.AddHealthChecks()
-    .AddDbContextCheck<GeradorDeProvasDbContext>(
-        name: "database_check",
-        failureStatus: HealthStatus.Unhealthy,
-        tags: ["ready"]
-    );
-
-var app = builder.Build();
-
-// Aplica migrações automaticamente em Desenvolvimento
-if (app.Environment.IsDevelopment())
+public partial class Program
 {
-    using var scope = app.Services.CreateScope();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-    var dbContext = scope.ServiceProvider.GetRequiredService<GeradorDeProvasDbContext>();
+        // Configuração do container de injeção de dependência
+        builder.Services.AddInfraRepositories(builder.Configuration, builder.Logging);
+        builder.Services.AddApplicationServices(builder.Configuration);
+        builder.Services.AddPresentationConfig(builder.Configuration);
 
-    dbContext.Database.Migrate();
+        // Configura health checks do banco de dados
+        builder.Services.AddHealthChecks()
+            .AddDbContextCheck<GeradorDeProvasDbContext>(
+                name: "database_check",
+                failureStatus: HealthStatus.Unhealthy,
+                tags: ["ready"]
+            );
+
+        var app = builder.Build();
+
+        // Aplica migrações automaticamente em Desenvolvimento
+        if (app.Environment.IsDevelopment())
+        {
+            using var scope = app.Services.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<GeradorDeProvasDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+
+        // Middlewares de roteamento
+        app.UseRouting();
+
+        // Middlewares de Auth
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        // Middleware de reconhecimento de rotas de controllers
+        app.MapDefaultControllerRoute();
+
+        // Execução do Servidor
+        app.Run();
+    }
 }
-
-// Middlewares de roteamento
-app.UseRouting();
-
-// Middlewares de Auth
-app.UseAuthentication();
-app.UseAuthorization();
-
-// Middleware de reconhecimento de rotas de controllers
-app.MapDefaultControllerRoute();
-
-// Execução do Servidor
-app.Run();
